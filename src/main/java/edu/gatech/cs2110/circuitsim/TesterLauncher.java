a973e51171cec5e083094d67dda37d84886ac23f
Austin Adams <git@austinjadams.com>
2018-06-01 18:30:49 -0400

    Build a Launcher instead of using ConsoleLauncher
    Students find the output of the ConsoleLauncher used last semester super
    confusing (heard on pizza "what is a container? ahhhh" 1,000,000 times),
    so build our own Launcher.

--- src/main/java/edu/gatech/cs2110/circuitsim/TesterLauncher.java
+++ src/main/java/edu/gatech/cs2110/circuitsim/TesterLauncher.java
@@ -1,15 +1,17 @@
 package edu.gatech.cs2110.circuitsim;
 
-import org.junit.platform.launcher.core.LauncherFactory;
-import org.junit.platform.launcher.core.Launcher;
-import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
-import org.junit.platform.launcher.core.LauncherDiscoveryRequest;
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
 
-//import org.junit.platform.console.ConsoleLauncher;
+import org.junit.platform.engine.TestExecutionResult;
+import org.junit.platform.launcher.TestExecutionListener;
+import org.junit.platform.launcher.TestIdentifier;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 
 public class TesterLauncher {
     public static void main(String[] args) {
-        //ConsoleLauncher.main("--details", "none", "--select-package", "edu.gatech.cs2110.circuitsim.tests");
         System.exit(new TesterLauncher().execute());
     }
 
@@ -17,11 +19,21 @@ public class TesterLauncher {
 
     private int execute() {
         Launcher launcher = LauncherFactory.create();
+        launcher.execute(buildDiscoveryRequest(), new TestListener());
         return 0;
     }
 
     private LauncherDiscoveryRequest buildDiscoveryRequest() {
         LauncherDiscoveryRequestBuilder builder = new LauncherDiscoveryRequestBuilder();
+        builder.selectors(selectPackage("edu.gatech.cs2110.circuitsim.tests"));
         return builder.build();
+    }
+
+    private class TestListener implements TestExecutionListener {
+        public void executionFinished(TestIdentifier testIdentifier,
+                                      TestExecutionResult testExecutionResult) {
+            System.err.printf("test %s: %s\n", testIdentifier.getDisplayName(),
+                                               testExecutionResult.getStatus());
+        }
     }
 }
