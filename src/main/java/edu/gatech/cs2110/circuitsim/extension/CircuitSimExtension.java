73c8240721ec2755e090744c3345fc71d272be23
Austin Adams <git@austinjadams.com>
2018-06-06 10:43:00 -0400

    Move CircuitSim API mess to new Subcircuit class

--- src/main/java/edu/gatech/cs2110/circuitsim/extension/CircuitSimExtension.java
+++ src/main/java/edu/gatech/cs2110/circuitsim/extension/CircuitSimExtension.java
@@ -1,18 +1,11 @@
 package edu.gatech.cs2110.circuitsim.extension;
 
-import java.io.File;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
-import com.ra4king.circuitsim.gui.CircuitSim;
-import com.ra4king.circuitsim.gui.CircuitBoard;
-import com.ra4king.circuitsim.gui.Properties;
-import com.ra4king.circuitsim.gui.peers.wiring.PinPeer;
-//import com.ra4king.circuitsim.simulator.Circuit;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -23,11 +16,7 @@ import org.junit.jupiter.api.extension.ParameterResolver;
 public class CircuitSimExtension implements Extension, BeforeAllCallback, BeforeEachCallback,
                                             ParameterResolver {
     private List<FieldInjection> fieldInjections;
-    // TODO: Put all this info about subcircuit state into a new class
-    //       called SubcircuitState or something, which we can pass into
-    //       wrappers like Input/OutputPin. Should be much cleaner
-    private CircuitBoard circuitBoard;
-    private CircuitSim circuitSim;
+    private Subcircuit subcircuit;
 
     @Override
     public void beforeAll(ExtensionContext context) throws Exception {
@@ -39,27 +28,16 @@ public class CircuitSimExtension implements Extension, BeforeAllCallback, Before
                 "The CircuitSim extension requires annotating the test class with @SubcircuitTest");
         }
 
-        File circuitFile = new File(subcircuitAnnotation.file());
-
-        // In theory, zucchini should guarantee this for us, but stuff
-        // happens
-        if (!circuitFile.canRead()) {
-            throw new IllegalArgumentException(
-                String.format("Cannot read circuit file `%s'", subcircuitAnnotation.file()));
-        }
-
-        circuitSim = new CircuitSim(false);
-        circuitSim.loadCircuits(circuitFile);
-        circuitBoard = lookupSubcircuit(circuitSim, subcircuitAnnotation.subcircuit());
-
+        subcircuit = Subcircuit.fromPath(subcircuitAnnotation.file(),
+                                         subcircuitAnnotation.subcircuit());
         fieldInjections = new LinkedList<>();
-        fieldInjections.addAll(lookupPins(circuitBoard, testClass));
+        fieldInjections.addAll(generatePinFieldInjections(testClass));
     }
 
     @Override
     public void beforeEach(ExtensionContext extensionContext) throws Exception {
         // Reset simulator before each test
-        circuitSim.getSimulator().reset();
+        subcircuit.resetSimulation();
 
         // Do simple dependency injection
         for (FieldInjection fieldInjection : fieldInjections) {
@@ -78,41 +56,14 @@ public class CircuitSimExtension implements Extension, BeforeAllCallback, Before
         return null;
     }
 
-    private String canonicalName(String name) {
-        return name.toLowerCase().replaceAll("[^0-9a-z]+", "");
-    }
-
-    private CircuitBoard lookupSubcircuit(CircuitSim circuitSim, String subcircuitName) {
-        String canonicalSubcircuitName = canonicalName(subcircuitName);
-        Map<String, CircuitBoard> boards = circuitSim.getCircuitBoards();
-        List<CircuitBoard> matchingCircuits = boards.entrySet().stream()
-            .filter(entry -> canonicalName(entry.getKey()).equals(canonicalSubcircuitName))
-            .map(Map.Entry::getValue)
-            .collect(Collectors.toList());
-
-        if (matchingCircuits.size() == 0) {
-            throw new IllegalArgumentException(String.format(
-                "No subcircuits match the name `%s'." +
-                "Please double-check the names of all your subcircuits.",
-                subcircuitName));
-        } else if (matchingCircuits.size() > 1) {
-            throw new IllegalArgumentException(String.format(
-                "More than one subcircuit has the name `%s'." +
-                "Can't continue deterministically.",
-                subcircuitName));
-        }
-
-        return matchingCircuits.get(0);
-    }
-
-    private List<FieldInjection> lookupPins(CircuitBoard circuitBoard, Class<?> testClass) {
+    private List<FieldInjection> generatePinFieldInjections(Class<?> testClass) {
         List<FieldInjection> fieldInjections = new LinkedList<>();
 
         List<Field> pinFields = Arrays.stream(testClass.getDeclaredFields())
                                       .filter(field -> field.isAnnotationPresent(SubcircuitPin.class))
                                       .collect(Collectors.toList());
 
-        // TODO: Detect duplicate pins (`Pin a' and `Pin b' are distinct
+        // TODO: Detect duplicate pins (`Pin a' and `Pin A' are distinct
         //       fields in Java but not here). Easy solution: sort the
         //       stream by canonicalName(field.getName()) and iterate
         //       over the resulting List, comparing each with the
@@ -126,43 +77,9 @@ public class CircuitSimExtension implements Extension, BeforeAllCallback, Before
 
             boolean wantInputPin = InputPin.class.isAssignableFrom(pinField.getType());
             SubcircuitPin pinAnnotation = pinField.getDeclaredAnnotation(SubcircuitPin.class);
-            String canonicalPinLabel = canonicalName(pinAnnotation.value().isEmpty()? pinField.getName()
-                                                                                    : pinAnnotation.value());
+            String pinLabel = pinAnnotation.value().isEmpty()? pinField.getName() : pinAnnotation.value();
 
-            // TODO: Search circuit for the pins we want
-            List<PinPeer> matchingPins = circuitBoard.getComponents().stream()
-                .filter(component -> component instanceof PinPeer &&
-                        ((PinPeer) component).getProperties().containsProperty(Properties.LABEL) &&
-                        canonicalPinLabel.equals(
-                            canonicalName(((PinPeer) component).getProperties().getValue(Properties.LABEL))))
-                .map(component -> (PinPeer) component)
-                .collect(Collectors.toList());
-
-            if (matchingPins.size() > 1) {
-                throw new IllegalArgumentException(String.format(
-                    "Subcircuit `%s' contains %d input/output pins labelled `%s', expected 1",
-                    circuitBoard.getCircuit().getName(), matchingPins.size(), canonicalPinLabel));
-            } else if (matchingPins.isEmpty()) {
-                throw new IllegalArgumentException(String.format(
-                    "Subcircuit `%s' contains no input/output pins labelled `%s'!",
-                    circuitBoard.getCircuit().getName(), canonicalPinLabel));
-            }
-
-            PinPeer matchingPin = matchingPins.get(0);
-
-            if (matchingPin.isInput() != wantInputPin) {
-                throw new IllegalArgumentException(String.format(
-                    "Subcircuit `%s' has %s pin labelled `%s', but expected it to be an %s pin instead",
-                    circuitBoard.getCircuit().getName(),
-                    // Use their label to be less confusing
-                    matchingPin.getProperties().getValue(Properties.LABEL),
-                    matchingPin.isInput()? "input" : "output",
-                    wantInputPin? "input" : "output"));
-            }
-
-            BasePin pinWrapper = wantInputPin? new InputPin(matchingPin.getComponent())
-                                             : new OutputPin(matchingPin.getComponent());
-
+            BasePin pinWrapper = subcircuit.lookupPin(pinLabel, wantInputPin);
             fieldInjections.add(new FieldInjection(pinField, pinWrapper));
         }
 
---
+++ src/main/java/edu/gatech/cs2110/circuitsim/extension/Subcircuit.java
@@ -0,0 +1,109 @@
+package edu.gatech.cs2110.circuitsim.extension;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.ra4king.circuitsim.gui.CircuitSim;
+import com.ra4king.circuitsim.gui.CircuitBoard;
+import com.ra4king.circuitsim.gui.Properties;
+import com.ra4king.circuitsim.gui.peers.wiring.PinPeer;
+
+class Subcircuit {
+    private CircuitSim circuitSim;
+    private CircuitBoard circuitBoard;
+
+    public Subcircuit(CircuitSim circuitSim, CircuitBoard circuitBoard) {
+        this.circuitSim = circuitSim;
+        this.circuitBoard = circuitBoard;
+    }
+
+    // Don't hate me for `throws Exception', Roi made me do it
+    public static Subcircuit fromPath(String simFilePath, String subcircuitName) throws Exception {
+        File circuitFile = new File(simFilePath);
+
+        // In theory, zucchini should guarantee this for us, but stuff
+        // happens
+        // TODO: Race condition R I P
+        if (!circuitFile.canRead()) {
+            throw new IllegalArgumentException(
+                String.format("Cannot read circuit file `%s'", simFilePath));
+        }
+
+        CircuitSim circuitSim = new CircuitSim(false);
+        circuitSim.loadCircuits(circuitFile);
+
+        CircuitBoard circuitBoard = lookupSubcircuit(circuitSim, subcircuitName);
+
+        return new Subcircuit(circuitSim, circuitBoard);
+    }
+
+    private static String canonicalName(String name) {
+        return name.toLowerCase().replaceAll("[^0-9a-z]+", "");
+    }
+
+    private static CircuitBoard lookupSubcircuit(CircuitSim circuitSim, String subcircuitName) {
+        String canonicalSubcircuitName = canonicalName(subcircuitName);
+        Map<String, CircuitBoard> boards = circuitSim.getCircuitBoards();
+        List<CircuitBoard> matchingCircuits = boards.entrySet().stream()
+            .filter(entry -> canonicalName(entry.getKey()).equals(canonicalSubcircuitName))
+            .map(Map.Entry::getValue)
+            .collect(Collectors.toList());
+
+        if (matchingCircuits.size() == 0) {
+            throw new IllegalArgumentException(String.format(
+                "No subcircuits match the name `%s'." +
+                "Please double-check the names of all your subcircuits.",
+                subcircuitName));
+        } else if (matchingCircuits.size() > 1) {
+            throw new IllegalArgumentException(String.format(
+                "More than one subcircuit has the name `%s'." +
+                "Can't continue deterministically.",
+                subcircuitName));
+        }
+
+        return matchingCircuits.get(0);
+    }
+
+    public void resetSimulation() {
+        circuitSim.getSimulator().reset();
+    }
+
+    public BasePin lookupPin(String pinLabel, boolean wantInputPin) {
+        String canonicalPinLabel = canonicalName(pinLabel);
+        List<PinPeer> matchingPins = circuitBoard.getComponents().stream()
+            .filter(component -> component instanceof PinPeer &&
+                    ((PinPeer) component).getProperties().containsProperty(Properties.LABEL) &&
+                    canonicalPinLabel.equals(
+                        canonicalName(((PinPeer) component).getProperties().getValue(Properties.LABEL))))
+            .map(component -> (PinPeer) component)
+            .collect(Collectors.toList());
+
+        if (matchingPins.size() > 1) {
+            throw new IllegalArgumentException(String.format(
+                "Subcircuit `%s' contains %d input/output pins labelled `%s', expected 1",
+                circuitBoard.getCircuit().getName(), matchingPins.size(), canonicalPinLabel));
+        } else if (matchingPins.isEmpty()) {
+            throw new IllegalArgumentException(String.format(
+                "Subcircuit `%s' contains no input/output pins labelled `%s'!",
+                circuitBoard.getCircuit().getName(), canonicalPinLabel));
+        }
+
+        PinPeer matchingPin = matchingPins.get(0);
+
+        if (matchingPin.isInput() != wantInputPin) {
+            throw new IllegalArgumentException(String.format(
+                "Subcircuit `%s' has %s pin labelled `%s', but expected it to be an %s pin instead",
+                circuitBoard.getCircuit().getName(),
+                // Use their label to be less confusing
+                matchingPin.getProperties().getValue(Properties.LABEL),
+                matchingPin.isInput()? "input" : "output",
+                wantInputPin? "input" : "output"));
+        }
+
+        BasePin pinWrapper = wantInputPin? new InputPin(matchingPin.getComponent())
+                                         : new OutputPin(matchingPin.getComponent());
+        return pinWrapper;
+    }
+}
