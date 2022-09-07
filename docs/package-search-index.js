3f20b5b61420e9c79e77d2b6477b47792b1666ee
Austin Adams <git@austinjadams.com>
2022-09-07 14:54:36 -0400

    Regenerate javadoc with Java 14

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.zucchini.circuitsimtester.api"},{"l":"io.zucchini.circuitsimtester.extension"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.zucchini.circuitsimtester.api"},{"l":"io.zucchini.circuitsimtester.extension"}]
